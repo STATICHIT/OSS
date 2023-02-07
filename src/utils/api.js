74a36161fa1c155dd34f856f862f4a029508b0fc
STATICHIT <2394412110@qq.com>
2023-02-07 14:18:47 +0800

    封装接口方法

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,19 @@
 import http from './axios.js'
 
 let apiFun = {
+  //自定义对象，命名隔离，消除重名影响
+  user:{},
+  bucket:{
+    authorize:{},
+  },
+  object:{
+    tag:{},
+  },
 };
+
+/**
+ * 用户接口 
+ * */ 
 
 //登录
 apiFun.login = params => {
@@ -16,4 +28,120 @@ apiFun.enroll = params => {
 //创建RAM用户
 apiFun.newRAM = params => {
   return http.post('/user/createRam',params)
+}
+
+/**
+ * bucket接口
+ *  */
+
+//获取桶信息
+apiFun.bucket.get = (bucketName) => {
+  return http.get('/bucket/getBucketInfo？bucketName='+bucketName)
+}
+
+//获取桶列表
+apiFun.bucket.getList = () => {
+  return http.get('/bucket/listBuckets')
+}
+
+//创建一个桶
+apiFun.bucket.create = params => {
+  return http.post('/bucket/createBucket',params)
+}
+
+//删除一个桶
+apiFun.bucket.delete = (name) => {
+  return http.delete('/bucket/deleteBucket？bucketName='+name)
+}
+
+
+/**
+ * 收藏接口 
+ * */
+
+//用户收藏一个桶
+apiFun.bucket.collect = (name) =>{
+  return http.put('/favorite/putUserFavorite?bucketName='+name)
+}
+
+//用户删除一个桶
+apiFun.bucket.userDelete = (name) => {
+  return http.delete('/favorite/deleteUserFavorite?bucketName='+name)
+}
+
+/**
+ * bucket授权策略接口
+ *  */
+
+//添加/更新一个bucket授权策略
+apiFun.bucket.authorize.addOrUpdate = (bucketName,authorizeId) =>{
+  return http.post('/authorize/putAuthorize?bucketName='+bucketName+'&authorizeId='+authorizeId)
+}
+
+//获取权限策略列表
+apiFun.bucket.authorize.getList =(bucketName) =>{
+  return http.get('/authorize/listAuthorizes?bucketName='+bucketName)
+}
+
+//删除一个授权策略
+apiFun.bucket.authorize.delete =(bucketName,authorizeId) =>{
+  return http.delete('/authorize/deleteAuthorize?bucketName='+bucketName+'&authorizeId='+authorizeId)
+}
+
+/**
+ * 对象接口
+ *  */
+
+//从桶中获取一个对象的元数据
+apiFun.object.metadata = (objectName,bucketName) => {
+  return http.get('/ossObject/getObjectInfo?objectName='+objectName+'&bucketName='+bucketName)
+}
+
+//从桶中获取一个对象的真实数据
+apiFun.object.dataInfo = (objectName,bucketName) => {
+  return http.get('/ossObject/getObject?objectName='+objectName+'&bucketName='+bucketName)
+}
+
+//在桶中添加一个文件夹
+apiFun.object.add = (bucketName,objectName,parentObject) =>{
+  return http.put('/ossObject/putFolder?bucketName='+bucketName+'&objectName='+objectName+'&parentObjectId='+parentObject)
+}
+
+//在桶中添加一个对象[小文件]
+apiFun.object.addSmall = (bucketName,objectName,md5,parentObject) =>{
+  return http.put('/ossObject/putSmallObject?bucketName='+bucketName+'&objectName='+objectName+'&md5='+md5+'&parentObjectId='+parentObject)
+}
+
+//在桶中添加一个对象[大文件]
+apiFun.object.addBig = (bucketName,objectName,md5,size,chunks,chunk,parentObject) =>{
+  return http.put('/ossObject/putBigObject?bucketName='+bucketName+'&objectName='+objectName+'&md5='+md5+'&size='+size+'&chunks='+chunks+'&chunk='+chunk+'&parentObjectId='+parentObject)
+}
+
+//从桶中删除一个对象
+apiFun.object.delete = (bucketName,objectName) => {
+  return http.delete('/ossObject/deleteObject?bucketName='+bucketName+'&ObjectName='+objectName)
+}
+
+//获取对象列表
+apiFun.object.objectList = (bucketName,key,pagenum,size,parentObjectId) => {
+  return http.get('/ossObject/listObjects?bucketName='+bucketName+'&key='+key+'&pagenum='+pagenum+'&size='+size+'&parentObjectId='+parentObjectId)
+}
+
+/**
+ * 对象标签接口
+ */
+
+//获取对象标签
+apiFun.object.tag.get =(objectName) =>{
+  return http.get('/objectTag/getObjectTag?objectName='+objectName)
+}
+
+//添加对象标签
+apiFun.object.tag.add =(objectName) =>{
+  return http.put('/objectTag/putObjectTag?objectName='+objectName)
+}
+
+//删除对象标签
+apiFun.object.tag.delete =(objectName,key) =>{
+  return http.delete('/objectTag/deleteObjectTag?objectName='+objectName+'&key='+key)
 }
