75ce4bc3aac7ddf11a3ab896b418624e07552861
STATICHIT <2394412110@qq.com>
2023-02-07 13:10:06 +0800

    设置调用接口方法的全局变量apiFun

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 import App from './App.vue'
-import axios from 'axios'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/dist/index.css'
@@ -9,18 +8,17 @@ import router from './router/index'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './style.css'
 
+//把apiFun对象赋值给vue的全局变量
+import apiFun from './utils/api.js';
+Vue.config.productionTip = false;//阻止启动生产消息
+Vue.prototype.$apiFun = apiFun;//全局使用$apiFun调用接口方法
+
+
 const app = createApp(App) // 生成 Vue 实例 app
 
 app.use(router)
 
 app.use(createPinia())
 app.use(ElementPlus)
-
-app.config.globalProperties.$http = axios
-
-
-
-  axios.defaults.baseURL = 'http://101.35.43.156:8080';//后端开发环境地址
-
 
 createApp(App).mount('#app')
